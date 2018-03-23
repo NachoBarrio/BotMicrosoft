6d5722ddf92d96c3d8b7fc2076e9d29508b82619
Ignacio Barrio Santos <ibarrio@infoavan.com>
2018-03-23 14:31:29 +0100

    Dev

--- Bot Application/Dialogs/RootDialog.cs
+++ Bot Application/Dialogs/RootDialog.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
 
 namespace Bot_Application.Dialogs
 {
@@ -18,14 +20,31 @@ namespace Bot_Application.Dialogs
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
             var activity = await result as Activity;
-
+            if (activity.Text.ToLower().Contains("order"))
+            {
+                // User said 'order', so invoke the New Order Dialog and wait for it to finish.
+                // Then, call ResumeAfterNewOrderDialog.
+                await context.Forward(new NewOrderDialog(), this.ResumeAfterNewOrderDialog, activity, CancellationToken.None);
+            }
             // calculate something for us to return
             int length = (activity.Text ?? string.Empty).Length;
 
             // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+            //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
 
             context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task ResumeAfterNewOrderDialog(IDialogContext context, IAwaitable<string> result)
+        {
+            // Store the value that NewOrderDialog returned. 
+            // (At this point, new order dialog has finished and returned some value to use within the root dialog.)
+            var resultFromNewOrder = await result;
+
+            await context.PostAsync($"New order dialog just told me this: {resultFromNewOrder}");
+
+            // Again, wait for the next message from the user.
+            context.Wait(this.MessageReceivedAsync);
         }
     }
 }
